d9a507d3e2114c0cbc6c39b5333bcbba1b694052
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2023-04-07 14:41:25 +0100

    More changes

--- pvpmanager/src/main/java/me/NoChance/PvPManager/Listeners/PlayerListener.java
+++ pvpmanager/src/main/java/me/NoChance/PvPManager/Listeners/PlayerListener.java
@@ -37,7 +37,7 @@ import com.google.common.cache.CacheBuilder;
 
 import me.NoChance.PvPManager.PvPlayer;
 import me.NoChance.PvPManager.Dependencies.Hook;
-import me.NoChance.PvPManager.Dependencies.API.WorldGuardDependency;
+import me.NoChance.PvPManager.Dependencies.Interfaces.WorldGuardDependency;
 import me.NoChance.PvPManager.Managers.PlayerHandler;
 import me.NoChance.PvPManager.Player.ProtectionResult;
 import me.NoChance.PvPManager.Settings.Messages;
@@ -59,9 +59,9 @@ public class PlayerListener implements Listener {
 	public PlayerListener(final PlayerHandler ph) {
 		this.ph = ph;
 		this.wg = (WorldGuardDependency) ph.getPlugin().getDependencyManager().getDependency(Hook.WORLDGUARD);
-		if (CombatUtils.isMCVersionAtLeast(MCVersion.V1_13)) {
+		if (MCVersion.isAtLeast(MCVersion.V1_13)) {
 			mushroomSoup = Material.MUSHROOM_STEW;
-		} else if (CombatUtils.isMCVersionAtLeast(MCVersion.V1_8)) { // avoid loading Material class on unit tests
+		} else if (MCVersion.isAtLeast(MCVersion.V1_8)) { // avoid loading Material class on unit tests
 			mushroomSoup = Material.getMaterial("MUSHROOM_SOUP");
 		}
 	}
@@ -133,7 +133,7 @@ public class PlayerListener implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR)
 	public final void onPlayerLogoutMonitor(final PlayerQuitEvent event) {
-		Bukkit.getScheduler().runTask(ph.getPlugin(), () -> ph.removeUser(ph.get(event.getPlayer())));
+		ph.removeUser(ph.get(event.getPlayer()));
 	}
 
 	@EventHandler(priority = EventPriority.HIGH)
@@ -198,7 +198,7 @@ public class PlayerListener implements Listener {
 					player.getHealth() + Settings.getSoupHealth() > player.getMaxHealth() ? player.getMaxHealth()
 							: player.getHealth() + Settings.getSoupHealth());
 			if (Settings.isSoupBowlDisappear()) {
-				if (CombatUtils.isMCVersionAtLeast(MCVersion.V1_9)) {
+				if (MCVersion.isAtLeast(MCVersion.V1_9)) {
 					player.getInventory().getItemInMainHand().setAmount(0);
 				} else {
 					player.getInventory().setItemInHand(null);
@@ -295,7 +295,7 @@ public class PlayerListener implements Listener {
 		if (event.getCause().equals(TeleportCause.ENDER_PEARL) && Settings.isBlockEnderPearl()) {
 			event.setCancelled(true);
 			pvplayer.message(Messages.getEnderpearlBlockedIncombat());
-		} else if (CombatUtils.isMCVersionAtLeast(MCVersion.V1_9) && event.getCause() == TeleportCause.CHORUS_FRUIT
+		} else if (MCVersion.isAtLeast(MCVersion.V1_9) && event.getCause() == TeleportCause.CHORUS_FRUIT
 		        && Settings.isBlockChorusFruit()) {
 			event.setCancelled(true);
 			pvplayer.message(Messages.getChorusBlockedInCombat());
