e716a8dfed19d7d20d39d9aa61377b82d94eb9d0
ChanceSD <5256215+ChanceSD@users.noreply.github.com>
2024-04-30 17:06:29 +0100

    Move to new package

---
+++ pvpmanager/src/main/java/me/chancesd/pvpmanager/tasks/PvPToggleFeeTask.java
@@ -0,0 +1,23 @@
+package me.chancesd.pvpmanager.tasks;
+
+import me.NoChance.PvPManager.PvPlayer;
+import me.chancesd.pvpmanager.manager.PlayerHandler;
+
+public class PvPToggleFeeTask implements Runnable {
+
+	private final PlayerHandler ph;
+
+	public PvPToggleFeeTask(final PlayerHandler ph) {
+		this.ph = ph;
+	}
+
+	@Override
+	public final void run() {
+		for (final PvPlayer p : ph.getPlayers().values()) {
+			if (!p.hasPvPEnabled()) {
+				p.applyPvPDisabledFee();
+			}
+		}
+	}
+
+}
